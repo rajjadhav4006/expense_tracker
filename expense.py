c078ec437cd4adf332c6f3a060e32c531c69a511
rajjadhav4006 <161046174+rajjadhav4006@users.noreply.github.com>
2026-04-24 10:58:58 +0530

    Refactor Expense class and remove unused code
    Removed unused imports and the analyze_expenses_df function.

--- expense.py
+++ expense.py
@@ -1,7 +1,4 @@
-import pandas as pd
-from db import connect
-
-def analyze_expenses_df():class Expense:
+class Expense:
     def __init__(self, title, amount, category, date):
         self.title = title
         self.amount = amount
@@ -10,7 +7,3 @@ def analyze_expenses_df():class Expense:
 
     def to_tuple(self):
         return (self.title, self.amount, self.category, self.date)
-    conn = connect()
-    df = pd.read_sql_query("SELECT * FROM expenses", conn)
-    conn.close()
-    return df
