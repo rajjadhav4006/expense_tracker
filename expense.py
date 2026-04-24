8a065fb280d89c0db5c641040d0b6ceef5ee0599
rajjadhav4006 <161046174+rajjadhav4006@users.noreply.github.com>
2026-04-24 10:53:51 +0530

    Update expense.py

--- expense.py
+++ expense.py
@@ -1,7 +1,15 @@
 import pandas as pd
 from db import connect
 
-def analyze_expenses_df():
+def analyze_expenses_df():class Expense:
+    def __init__(self, title, amount, category, date):
+        self.title = title
+        self.amount = amount
+        self.category = category
+        self.date = date
+
+    def to_tuple(self):
+        return (self.title, self.amount, self.category, self.date)
     conn = connect()
     df = pd.read_sql_query("SELECT * FROM expenses", conn)
     conn.close()
